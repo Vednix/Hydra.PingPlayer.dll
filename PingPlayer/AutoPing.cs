253b2b1d2c8dd778476e6988302e93dd80b4a413
Vednix <contato@terrariabrasil.com.br>
2020-07-19 10:39:28 -0300

    Init

---
+++ PingPlayer/AutoPing.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using System.Net.NetworkInformation;
+using PingPlayer.Extensions;
+
+namespace PingPlayer
+{
+    public class AutoPing
+    {
+        protected static string BlankLine(int number)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < number; i++)
+            {
+                sb.Append("\r\n");
+            }
+
+            return sb.ToString();
+        }
+        public static async void Run()
+        {
+            await Task.Run(async () =>
+             {
+                 await PingPlayerRun();
+             });
+        }
+        public static async Task PingPlayerRun()
+        {
+            while (true)
+            {
+                //SynchronizationContext.SetSynchronizationContext(new SynchronizationContext());
+                //await Task.Factory.StartNew(() => Task.Delay(10000)).ContinueWith((t) =>
+                //{
+                Parallel.ForEach(TSPlayerExt.tsArray.Where(p => p != null && p.Active && !string.IsNullOrEmpty(p.IP)), player =>
+                {
+                    if (PlayerPing.Wait)
+                        return;
+                    Ping p = new Ping();
+
+                    string ping = p.Send(player.IP).RoundtripTime.ToString();
+                    if (player.IP == "127.0.0.1" || player.IP.StartsWith("10.0.") || player.IP.StartsWith("192.168.") && ping == "0")
+                        ping = "<1";
+
+                    if (ping != "0")
+                    {
+                        if (ping != "<1" && Int32.Parse(ping) >= 230 && !TSPlayerExt.WarnPingRed[player.Index])
+                        {
+                            if (player.IsPortuguese)
+                                player.SendErrorMessage("Seu tempo de resposta com o servidor está acima de 230ms, lag será notado em diversos momentos.");
+                            else
+                                player.SendErrorMessage("Your response time with the server is above 230ms, lag will be noticed in several moments.");
+                            TSPlayerExt.WarnPingRed[player.Index] = true;
+                            TSPlayerExt.Pinged[player.Index] = false;
+                        }
+                        if (ping != "<1" && Int32.Parse(ping) >= 120 && !TSPlayerExt.WarnPingOrange[player.Index] && !TSPlayerExt.WarnPingRed[player.Index])
+                        {
+                            if (player.IsPortuguese)
+                                player.SendWarningMessage("Seu tempo de resposta com o servidor está acima de 120ms, lag poderá ser notado em alguns momentos.");
+                            else
+                                player.SendWarningMessage("Your response time with the server is above 120ms, lag may be noticed in a few moments.");
+                            TSPlayerExt.WarnPingOrange[player.Index] = true;
+                            TSPlayerExt.Pinged[player.Index] = false;
+                        }
+
+                        if (!TSPlayerExt.Pinged[player.Index])
+                        {
+                            player.SendInfoMessage($"AutoPing: {ping}ms");
+                            TSPlayerExt.Pinged[player.Index] = true;
+                        }
+
+                        int lines = 8;
+                        string blanks = BlankLine(2);
+                        int bytes = 0;
+                        if (!player.Mobile)
+                        {
+                            lines = 20;
+                            blanks = BlankLine(60);
+                            bytes = 17;
+                        }
+                        string message = string.Format($"{BlankLine(lines)}AutoPing: {ping}ms{blanks}");
+                        if (TSPlayerExt.PingStatus[player.Index])
+                            player.SendData(PacketTypes.Status, message, bytes);
+                        if (TSPlayerExt.PingChat[player.Index])
+                            player.SendInfoMessage($"AutoPing {ping}ms");
+                        p.Dispose();
+                    }
+                });
+                //}, TaskScheduler.FromCurrentSynchronizationContext());
+                await Task.Delay(12000);
+            }
+        }
+    }
+}
---
+++ PingPlayer/Main.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Xna.Framework;
+using PingPlayer.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
+using TShockAPI.Hooks;
+
+namespace PingPlayer
+{
+    [ApiVersion(2, 1)]
+    public class PlayerPing : TerrariaPlugin
+    {
+        public override Version Version => new Version(1, 0, 0, 0);
+
+        public override string Name
+        {
+            get { return "PingPlayer"; }
+        }
+
+        public override string Author
+        {
+            get { return "Vednix"; }
+        }
+
+        public override string Description
+        {
+            get { return "A plugin to send their player pings to them"; }
+        }
+
+        public PlayerPing(Main game) : base(game)
+        {
+            Order = 1;
+        }
+        internal static bool Wait = false;
+        public override void Initialize()
+        {
+            Commands.ChatCommands.Add(new Command(PingCmd, "ping"));
+            ServerApi.Hooks.GamePostInitialize.Register(this, PostInitialize);
+            ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
+            ServerApi.Hooks.NetGreetPlayer.Register(this, OnGreet);
+        }
+        public static bool[] internalPlayer;
+        private static void PostInitialize(EventArgs args)
+        {
+            AutoPing.Run();
+        }
+        private static void OnJoin(JoinEventArgs args)
+        {
+            Wait = true;
+            TSPlayerExt.Reset(args.Who);
+        }
+        private static void OnGreet(GreetPlayerEventArgs args)
+        {
+            Wait = false;
+        }
+        private static void PingCmd(CommandArgs args)
+        {
+            if (args.Parameters.Count > 0)
+            {
+                switch (args.Parameters[0])
+                {
+                    case "togglestatus":
+                    case "status":
+                        TSPlayerExt.PingStatus[args.Player.Index] = !TSPlayerExt.PingStatus[args.Player.Index];
+                        TSPlayerExt.PingChat[args.Player.Index] = false;
+                        if (args.Player.IsPortuguese)
+                            args.Player.SendSuccessMessage(string.Format("Agora você {0} receber ping através de Status", TSPlayerExt.PingStatus[args.Player.Index] ? "[c/98C807:irá]" : "[c/ffa500:não irá]"));
+                        else
+                            args.Player.SendSuccessMessage(string.Format("Now you {0} receive ping via Status", TSPlayerExt.PingStatus[args.Player.Index] ? "[c/98C807:will]" : "[c/ffa500:will not]"));
+                        break;
+                    case "togglechat":
+                    case "chat":
+                        TSPlayerExt.PingChat[args.Player.Index] = !TSPlayerExt.PingChat[args.Player.Index];
+                        TSPlayerExt.PingStatus[args.Player.Index] = false;
+                        if (args.Player.IsPortuguese)
+                            args.Player.SendSuccessMessage(string.Format("Agora você {0} receber ping através do Chat", TSPlayerExt.PingChat[args.Player.Index] ? "[c/98C807:irá]" : "[c/ffa500:não irá]"));
+                        else
+                            args.Player.SendSuccessMessage(string.Format("Now you {0} receive ping via Chat", TSPlayerExt.PingChat[args.Player.Index] ? "[c/98C807:will]" : "[c/ffa500:will not]"));
+                        break;
+                    case "me":
+                        Ping p = new Ping();
+                        string ping = p.Send(args.Player.IP).RoundtripTime.ToString();
+                        if (int.Parse(ping) != 0)
+                            args.Player.SendInfoMessage($"Ping {ping}ms");
+                        else
+                        {
+                            if (args.Player.IsPortuguese)
+                                args.Player.SendErrorMessage("Seu provedor de internet não permite o servidor medir seu ping");
+                            else
+                                args.Player.SendErrorMessage("Your ISP does not allow the server to measure your ping");
+                        }
+                        break;
+                    default:
+                        ShowCmds(args);
+                        break;
+                }
+            }
+            else
+                ShowCmds(args);
+        }
+        private static void ShowCmds(CommandArgs args)
+        {
+            if (args.Player.IsPortuguese)
+            {
+                args.Player.SendMessage("PingPlayer - Comandos", Color.Magenta);
+                args.Player.SendMessage("[c/ffd700:/ping status] => Ativa ou desativa o envio automatico de pings através de Status", Color.LightGray);
+                args.Player.SendMessage("[c/ffd700:/ping chat] => Ativa ou desativa o envio automatico de pings através do Chat", Color.LightGray);
+                args.Player.SendMessage("[c/ffd700:/ping me] => Ver seu ping uma única vez apenas", Color.LightGray);
+            }
+            else
+            {
+                args.Player.SendMessage("PingPlayer - Commands", Color.Magenta);
+                args.Player.SendMessage("[c/ffd700:/ping togglestatus] => Enables or disables automatic pinging via Status", Color.LightGray);
+                args.Player.SendMessage("[c/ffd700:/ping togglechat] => Enables or disables automatic pinging via Chat", Color.LightGray);
+                args.Player.SendMessage("[c/ffd700:/ping me] => See your ping only once", Color.LightGray);
+            }
+        }
+    }
+}
